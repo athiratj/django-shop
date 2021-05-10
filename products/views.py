a762fe81aad5389dcba1b557038f67c40b80015f
athira_tj <52416790+athiratj@users.noreply.github.com>
2021-05-10 23:17:22 +0530

    including model in view for public to see using dummy data in products/templates/index.html

--- products/views.py
+++ products/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Welcome to Django project</h1>")
+    return render(request, 'index.html')
+    # return HttpResponse("<h1>Welcome to Django project</h1>")
 
 def about(request):
     return HttpResponse("<h1>About page.</h1>")
